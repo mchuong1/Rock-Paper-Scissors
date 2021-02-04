68941368eb085feab62c1ba5220caab3099e2674
Matthew Chuong <mstchuong@yahoo.com>
2021-02-03 22:16:08 -0500

    :100: Add onChooseType
    -sets player choice and computer choice on click

--- src/components/RockPaperScissor.js
+++ src/components/RockPaperScissor.js
@@ -1,9 +1,17 @@
 
-
 export default function RockPaperScissor(props) {
+  const choices = ["Rock", "Paper", "Scissors"]
+
+  const setChoices = () =>{ 
+    let randomChoice = Math.floor(3 * Math.random())
+    props.setPlayerChoice(props.type)
+    props.setComputerChoice(choices[randomChoice])
+  }
+
   return (
     <div className="RockPaperScissor">
-        <img alt="" src={props.img} className="circle-image"></img>
+        <img alt={props.type} src={props.img} className="circle-image" onClick={setChoices}></img>
     </div>
   );
 }
+
